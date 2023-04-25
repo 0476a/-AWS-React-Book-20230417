159062236bb7f5da0eb67b3ae598ce511e444dfd
0476a <0476a@naver.com>
2023-04-25 17:54:57 +0900

    2023.04.25(도서 전체 조회)

--- book_management/src/pages/Main/Main.js
+++ book_management/src/pages/Main/Main.js
@@ -1,9 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import BookCard from '../../components/UI/BookCard/BookCard';
 import axios from 'axios';
+import { useQuery } from 'react-query';
+import { BsMenuDown } from 'react-icons/bs';
 
 const mainContainer = css`
     padding: 10px;
@@ -12,7 +14,53 @@ const mainContainer = css`
 const header = css`
     display: flex;
     justify-content: space-between;
+    padding: 40px;
     height: 100px;
+`;
+
+const title = css`
+    font-size: 35px;
+    font-weight: 600;
+`;
+
+const searchItems = css`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+`;
+
+const categoryButton = css`
+    position: relative;
+    border: 1px solid #dbdbdb;
+    border-radius: 5px;
+    width: 30px;
+    height: 30px;
+    background-color: white;
+    cursor: pointer;
+`;
+
+const categoryGroup = (isOpen) => css`
+    position: absolute;
+    top: 30px;
+    right: -151px;
+    display: ${isOpen ? "flex" : "none"};
+    flex-direction: column;
+    align-items: flex-start;
+    border: 1px solid #dbdbdb;
+    border-radius: 4px;
+    padding: 5px;
+    width: 180px;
+    max-height: 100px;
+    background-color: white;
+    overflow-y: auto;
+`;
+
+const searchInput = css`
+    border: 1px solid #dbdbdb;
+    border-radius: 7px;
+    padding: 5px;
+    width: 150px;
+    height: 30px;
 `;
 
 const main = css`
@@ -22,41 +70,120 @@ const main = css`
     overflow-y: auto;
 `;
 
-
 const Main = () => {
+    const [ searchParam, setSearchParam ] = useState({page: 1, searchValue: "", categoryIds: []})
+    const [ refresh, setRefresh ] = useState(false);
+    const [ categoryRefresh, setCategoryRefresh ] = useState(true);
+    // books에 책 정보들이 쌓일 것이다.
+    const [ books, setBooks ] = useState([]);
+    const [ lastPage, setLastPage ] = useState(1);
+    const [ isOpen, setIsOpen ] = useState(false);
+    const lastBookRef = useRef();
+    const categoryButtonRef = useRef();
+
+    // 처음딱 마운트 될때만 이렇게 됨.
     useEffect(() => {
-        searchBooks();
+        const observerService = (entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    setRefresh(true);
+                }
+            });
+        }
+
+        const observer = new IntersectionObserver(observerService, {threshold: 1});
+        // ref 객체를 가지고 오려면 current를 꼭 적어줘야함!
+        observer.observe(lastBookRef.current);
     }, []);
 
-    const searchBooks = async () => {
-        const searchParam = {
-            page : 1,
-            searchValue: "",
-            categoryId: 0
+    
+    const option = {
+        params: searchParam,
+        headers: {
+            Authorization: localStorage.getItem("accessToken")
         }
+    }
+    const searchBooks = useQuery(["searchBooks"], async () => {
+        const response = await axios.get("http://localhost:8080/books", option);
+        return response;
+    }, {
+        // 성공적으로 받아 왔을 때
+        onSuccess: (response) => {
+            if(refresh) {
+                setRefresh(false);
+            }
+            console.log(response);
+            const totalCount = response.data.totalCount;
+            setLastPage(totalCount % 20 === 0 ? totalCount / 20 : Math.ceil(totalCount / 20));
+            setBooks([...books, ...response.data.bookList]);
+            setSearchParam({...searchParam, page:searchParam.page + 1})
+        },
+        // enabled가 true여야 useQuery 작동함!
+        enabled: refresh && searchParam.page < lastPage + 1
+    });
+
+    const categories = useQuery(["categories"], async () => {
         const option = {
-            
+            headers: {
+                Authorization: localStorage.getItem("accessToken")
+            }
         }
-        const response = await axios.get("http://localhost:8080/books", {params: {...searchParam}})
-        console.log(response);
+
+        const response = await axios.get("http://localhost:8080/categories", option);
+        console.log(response)
+        return response;
+    }, {
+        enabled: categoryRefresh,
+        onSuccess: () => {
+            if(categoryRefresh) {
+                setCategoryRefresh(false);
+            }
+        }
+    })
+
+    const categoryClickHandle = (e) => {
+        e.stopPropagation();
+
+        if(isOpen && (e.target === categoryButtonRef.current)) {
+            setIsOpen(false);
+        }else {
+            setIsOpen(true);
+        }
+    }
+
+    const categoryCheckHandle = (e) => {
+        if(e.target.checked) {
+            setSearchParam({...searchParam, categoryIds: [...searchParam.categoryIds, e.target.value]});
+        }else {
+            setSearchParam({...searchParam, categoryIds: [...searchParam.categoryIds.filter(id => id !== e.target.vale)]});
+        }
+        setRefresh(true);
     }
 
     return (
         <div css={mainContainer}>
             <Sidebar></Sidebar>
             <header css={header}>
-                <div>도서검색</div>
-                <div>
-                    <input type="search" />
+                <div css={title}>도서검색</div>
+                <div css={searchItems}>
+                    <button css={categoryButton} onClick={categoryClickHandle} ref={categoryButtonRef}>
+                        <BsMenuDown />
+                        <div css={categoryGroup(isOpen)}>
+                            {categories.data !== undefined
+                                ? categories.data.data.map(category => 
+                                    (<div key={category.categoryId}>
+                                        <input type="checkbox" onChange={categoryCheckHandle} id={"ct-" + category.categoryId} value={category.categoryId} />
+                                        <label htmlFor={"ct-" + category.categoryId}>{category.categoryName}</label>
+                                    </div>))
+                                : ""}
+                        </div>
+                    </button>
+                    <input css={searchInput} type="search" />
                 </div>
             </header>
             <main css={main}>
-                <BookCard></BookCard>
-                <BookCard></BookCard>
-                <BookCard></BookCard>
-                <BookCard></BookCard>
-                <BookCard></BookCard>
-                <BookCard></BookCard>
+                {books.length > 0 ? books.map(book => (<BookCard key={book.bookId} book={book}></BookCard>)) : ""}
+                <div ref={lastBookRef}></div>
             </main>
         </div>
     );
