1938b32ed44e14e41276cbdae5ecb363091aa737
0476a <0476a@naver.com>
2023-04-27 14:48:54 +0900

    2023.04.27(추천 넣고 빼기)

--- book_management/src/pages/BookDetail/BookDetail.js
+++ book_management/src/pages/BookDetail/BookDetail.js
@@ -3,8 +3,9 @@ import { css } from '@emotion/react';
 import React from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { useParams } from 'react-router-dom';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
+import RentalList from '../../components/UI/BookDetail/RentalList/RentalList';
 
 const mainContainer = css`
     padding: 10px;
@@ -12,6 +13,8 @@ const mainContainer = css`
 
 const BookDetail = () => {
     const { bookId } = useParams();
+    // principal 키로 유저 정보들을 가지고 있음
+    const queryClient = useQueryClient();
 
     const getBook = useQuery(["getBook"], async () => {
         const option = {
@@ -33,6 +36,59 @@ const BookDetail = () => {
         return response;
     });
 
+    const getLikeStatus = useQuery(["getLikeStatus"], async () => {
+        const option = {
+            params: {
+                // queryClient에서 유저 정보를 가지고 온 것을 볼 수 있다.
+                userId: queryClient.getQueryData("principal").data.userId
+            },
+            headers: {
+                Authorization: localStorage.getItem("accessToken")
+            }
+        }
+        const response = await axios.get(`http://localhost:8080/book/${bookId}/like/status`, option);
+        return response;
+    });
+
+    // react query에서 데이터를 수정할 때 사용됨.
+    const setLike = useMutation(async() => {
+        const option = {
+            headers: {
+                "Content-Type" : "application/json",
+                Authorization: localStorage.getItem("accessToken")
+            }
+        }
+        return await axios.post(`http://localhost:8080/book/${bookId}/like`, JSON.stringify({
+            userId: queryClient.getQueryData("principal").data.userId
+        }), option);
+    }, {
+        onSuccess: () => {
+            // invalidateQueries 캐시를 싹 지우고 다시 받아와줌(요청을 다시보넴).
+            queryClient.invalidateQueries("getLikeCount");
+            queryClient.invalidateQueries("getLikeStatus");
+        }
+    });
+
+    const disLike = useMutation(async() => {
+        const option = {
+            params: {
+                // queryClient에서 유저 정보를 가지고 온 것을 볼 수 있다.
+                userId: queryClient.getQueryData("principal").data.userId
+            },
+            headers: {
+                Authorization: localStorage.getItem("accessToken")
+            }
+        }
+        return await axios.delete(`http://localhost:8080/book/${bookId}/like`, option);
+    }, {
+        onSuccess: () => {
+            // invalidateQueries 캐시를 싹 지우고 다시 받아와줌(요청을 다시보넴).
+            // 캐시가 유지되는 시간을 만료 해줌. -> 비워지면 유즈쿼리가 다시 값을 불러와줌.
+            queryClient.invalidateQueries("getLikeCount");
+            queryClient.invalidateQueries("getLikeStatus");
+        }
+    });
+
     // 해당 로직이 있어야 데이터가 불러오는 동안은 해당 값이 return 되게됨.
     if(getBook.isLoading) {
         return <div>불러오는 중...</div>
@@ -51,10 +107,14 @@ const BookDetail = () => {
                     <img src={getBook.data.data.coverImgUrl} alt={getBook.data.data.categoryName} />
                 </div>
                 <div>
-
+                    <RentalList bookId={bookId}/>
                 </div>
                 <div>
-                    <button>추천</button>
+                    {getLikeStatus.isLoading 
+                        ? "" 
+                        : getLikeStatus.data.data === 0 
+                            ? (<button onClick={() => {setLike.mutate()}}>추천하기</button>)
+                            : (<button onClick={() => {disLike.mutate()}}>추천취소</button>)}
                 </div>
             </main>
         </div>
